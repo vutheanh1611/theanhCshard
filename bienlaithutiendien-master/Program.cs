4f566d88cf583cd441f52aa75bed961c2f0ac5e1
vutheanh1611 <vutheanh121216112000@gmail.com>
2023-01-07 21:10:26 +0700

    bienlai

---
+++ bienlaithutiendien-master/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using tiendien;
+public class progam
+{
+    static void Main(string[] args)
+
+    {
+        KhachHangVN a = new KhachHangVN(1, "Le Minh Duc", "2022-07-22", 219, "Cá nhân");
+        KhachHangNN b = new KhachHangNN(2, "Nguyen Van An", "2022-09-21", 156, "US");
+        Console.WriteLine(a.ThanhTien);
+        Console.WriteLine(b.ThanhTienNN);
+
+    }
+}
